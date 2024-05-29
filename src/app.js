5255dacb2b0f51618223308dd291a5334eca5073
Usman257 <117535587+Usman257@users.noreply.github.com>
2024-05-29 15:16:12 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const simbolos = ["♦", "♥", "♠", "♣"];
+  const numeros = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let simboloElegido = simbolos[Math.floor(Math.random() * simbolos.length)];
+  let numeroElegido = numeros[Math.floor(Math.random() * numeros.length)];
+  let palo = document.querySelector("#palo");
+  palo.innerHTML = simboloElegido;
+  let cartas = document.querySelector("#cartas");
+  cartas.innerHTML = numeroElegido;
+  let palo2 = document.querySelector("#palo-2");
+  palo2.innerHTML = simboloElegido;
 };
